9baf18fe4efed7e3c630f64631624c288dd4e96e
felipemonsef10 <126120156+felipemonsef10@users.noreply.github.com>
2024-03-27 16:45:01 -0300

    crud pronto

---
+++ orcamento/src/main/kotlin/com/finan/orcamento/model/OrcamentoModel.java
@@ -0,0 +1,39 @@
+package com.finan.orcamento.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@Table(name = "orcamento")
+public class OrcamentoModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "quantidade_itens")
+    private int quantidadeItens;
+
+    @NotNull
+    @Column(name = "valor_orcamento")
+    private BigDecimal valorOrcamento;
+
+    @Column(name = "desconto_icms")
+    private BigDecimal descontoICMS;
+
+    public void setQuantidadeItens(int quantidadeItens) {
+        this.quantidadeItens = quantidadeItens;
+    }
+
+    public void setValorOrcamento(BigDecimal valorOrcamento) {
+        this.valorOrcamento = valorOrcamento;
+    }
+
+    public void setDescontoICMS(BigDecimal descontoICMS) {
+        this.descontoICMS = descontoICMS;
+    }
+}
